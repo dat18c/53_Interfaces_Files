382480f753e60ca236f3a013434f3cded5023b71
JarlTuxen <jart@kea.dk>
2018-05-01 15:52:38 +0200

    serializable

---
+++ kode/Car.java
@@ -0,0 +1,31 @@
+import java.io.*;
+public class Car implements Serializable{
+
+   private String model;
+   private int power;
+   
+   //constructor
+   public Car(String mod, int pow){
+      this.model = mod;
+      this.power = pow;
+   }
+   
+   //getters & setters
+   public void setPower(int pow){
+      this.power = pow;
+   }
+   
+   public int getPower(){
+      return power;
+   }
+   
+   //check nummerplade
+   public void checkModel(){
+      System.out.println("Model: " + model);
+   }
+   
+   //pæn print
+   public String toString(){
+      return model + ": " + power + " hk";
+   } 
+}
---
+++ kode/CarDemo.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class CarDemo{
+
+   public static void main(String[] args) throws IOException, ClassNotFoundException {
+      ArrayList<Car> carList = new ArrayList<Car>();
+      carList.add(new Car("Kea C'eed", 120));
+      carList.add(new Car("Skoda City", 55));
+      carList.add(new Car("Panzer Mk IV", 750));
+
+      FileOutputStream fo = new FileOutputStream("carList.ser");
+      ObjectOutputStream output = new ObjectOutputStream(fo);
+      output.writeObject(carList);
+      output.close();
+      fo.close();
+      System.out.println("Saved to carList.ser");
+      System.out.println(carList);      
+
+      ArrayList<Car> carShop = new ArrayList<Car>();
+      
+      FileInputStream fi = new FileInputStream("carList.ser");
+      ObjectInputStream input = new ObjectInputStream(fi);
+      carShop = (ArrayList<Car>) input.readObject();
+      input.close();
+      fi.close();
+      System.out.println("Read from carList.ser");
+      System.out.println(carShop);
+      
+   }
+
+
+}
