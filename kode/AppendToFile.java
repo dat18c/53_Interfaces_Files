60a70eea9fa7704cb38842fda7eceee3f7cf4aa6
JarlTuxen <jart@kea.dk>
2018-05-01 10:58:04 +0200

    append kommentar

--- kode/AppendToFile.java
+++ kode/AppendToFile.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class AppendToFile{
    public static void main(String[] args) throws IOException {
       File f = new File("test.txt");
-      FileWriter fw = new FileWriter(f, true);
+      FileWriter fw = new FileWriter(f, true); //true appender
       PrintWriter pw = new PrintWriter(fw);
       pw.println("Extra text");
       pw.println("Blank linie");
